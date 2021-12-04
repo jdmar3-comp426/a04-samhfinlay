0a99128b1ebb904e2497c187282a6a12071350fc
Sam Finlay <69521209+samhfinlay@users.noreply.github.com>
2021-12-04 10:45:40 -0500

    asd

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 // Define app using express
 var express = require("express")
-var app = express()
+var app = express();
 // Require database SCRIPT file
 var db = require("./database.js");
 // Require md5 MODULE
@@ -14,49 +14,49 @@ app.use(express.json());
 var HTTP_PORT = 5000;
 // Start server
 app.listen(HTTP_PORT, () => {
-    console.log("Server running on port %PORT%".replace("%PORT%",HTTP_PORT))
+	console.log("Server running on port %PORT%".replace("%PORT%", HTTP_PORT));
 });
 // READ (HTTP method GET) at root endpoint /app/
 app.get("/app/", (req, res, next) => {
-    res.json({"message":"Your API works! (200)"});
+	res.json({ "message": "Your API works! (200)" });
 	res.status(200);
 });
 
 // Define other CRUD API endpoints using express.js and better-sqlite3
 // CREATE a new user (HTTP method POST) at endpoint /app/new/
-app.get("/app/new", (req, res) => {   
+app.get("/app/new", (req, res) => {
 	const stmt = db.prepare("INSERT INTO userinfo (user, pass) VALUES (?, ?)");
-	var protected = md5(req.body.pass)
+	var protected = md5(req.body.pass);
 	let info = stmt.run(req.body.user, protected);
-	res.status(201).json({"message":"OK (201)"})
+	res.status(201).json({ message: "OK (201)". replace("%X%", info.changes).replace("%Y%", info.lastInserted )});
 });
 // READ a list of all users (HTTP method GET) at endpoint /app/users/
-app.get("/app/users", (req, res) => {	
+app.get("/app/users", (req, res) => {
 	const stmt = db.prepare("SELECT * FROM userinfo").all();
 	res.status(200).json(stmt);
 });
 
 // READ a single user (HTTP method GET) at endpoint /app/user/:id
-app.get("/app/user/:id", (req, res) => {   
+app.get("/app/user/:id", (req, res) => {
 	const stmt = db.prepare("SELECT * FROM userinfo WHERE id = ?");
-	const get = stmt.get(req.params.id)
+	const get = stmt.get(req.params.id);
 	res.status(200).json(get);
 });
 // UPDATE a single user (HTTP method PATCH) at endpoint /app/update/user/:id
-app.get("/app/update/user/:id", (req, res) => {   
+app.get("/app/update/user/:id", (req, res) => {
 	const stmt = db.prepare("UPDATE userinfo SET req.body.user = COALESCE(?,user), pass = COALESCE(?,pass) WHERE id = ?");
-	var protected = md5(req.body.pass)
+	var protected = md5(req.body.pass);
 	let info = stmt.run(req.body.user, protected, req.params.id);
-	res.status(200).json({message:"OK (200)"})
+	res.status(200).json({ message: "OK (200)" });
 });
 // DELETE a single user (HTTP method DELETE) at endpoint /app/delete/user/:id
-app.get("/app/delete/user/:id", (req, res) => {   
+app.get("/app/delete/user/:id", (req, res) => {
 	const stmt = db.prepare("DELETE FROM userinfo WHERE id = ?");
-	res.status(405).json({"message":"OK (405)"})
+	res.status(405).json({ "message": "OK (405)" });
 	console.log(info.changes);
 });
 // Default response for any other request
-app.use(function(req, res){
-	res.json({"message":"Endpoint not found. (404)"});
-    res.status(404);
+app.use(function (req, res) {
+	res.json({ "message": "Endpoint not found. (404)" });
+	res.status(404);
 });
